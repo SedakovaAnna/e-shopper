af3ae63a3b8c25f461f0f37c132e807e91c2010e
Anna Sedakova <tuzik198@mail.ru>
2018-08-09 10:19:38 +0300

    autoload

--- controllers/NewsController.php
+++ controllers/NewsController.php
@@ -1,6 +1,5 @@
 <?php
 
-include_once ROOT . '/models/News.php';
 /**
  * Description of NewsController
  *
--- controllers/ProductController.php
+++ controllers/ProductController.php
@@ -1,6 +1,4 @@
 <?php
-include_once '/models/Category.php';
-include_once '/models/Product.php';
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
